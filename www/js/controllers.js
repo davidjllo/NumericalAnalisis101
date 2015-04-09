71bbfd5f2a1b3425f9b7203272a27f19fa80ea83
jaguir11 <aguirre.juan1992@gmail.com>
2015-04-09 09:26:23 -0500

    2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,17 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('AboutCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('MethodsCtrl', function($scope, Chats) {
+  $scope.methods = [{name: 'Bisection', cov: 'E^2', ident: 'bisection'},
+  					{name: 'False Rule', cov: 'E^2+1', ident: 'falseRule'},
+  					{name: 'Fixed Point', cov: 'E^2+2', ident: 'FixedPnt'},
+  					{name: 'Secant', cov: 'E^2+3', ident: 'secant'},
+  					{name: 'Multiple Roots', cov: 'E^2+4', ident: 'multRoots'},];
 })
+
+
+.controller('BisectionCtrl', function($scope) {})
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
